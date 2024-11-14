72e8ff793b5c051941aecafc17135fd72b2fd179
Michal Marek <marekmichal1991@gmail.com>
2024-11-14 18:24:06 +0100

    1st commit, added multiple browsers with ability to add more through json file and refractored login pages into one class.

--- Test_Automation_Final_Task/Pages/LoginPage.cs
+++ Test_Automation_Final_Task/Pages/LoginPage.cs
@@ -2,31 +2,36 @@
 
 namespace Test_Automation_Final_Task.Pages
 {
-    public class LoginPageEmpty
+    internal class LoginPage
     {
         private static string Url { get; } = "https://www.saucedemo.com/";
+        readonly IWebDriver driver;
 
-        IWebDriver driver;
+        public LoginPage(IWebDriver driver) => this.driver = driver ?? throw new ArgumentException(nameof(driver));
 
-        public LoginPageEmpty(IWebDriver driver) => this.driver = driver ?? throw new ArgumentException(nameof(driver));
-
-        public LoginPageEmpty Open()
+        public LoginPage Open()
         {
             driver.Url = Url;
             return this;
         }
 
-        public void Login()
+        public void Login(string? username = null, string? password = null)
         {
             // Username
             var usernameField = driver.FindElement(By.Id("user-name"));
-
             usernameField.Clear();
+            if (!string.IsNullOrEmpty(username))
+            {
+                usernameField.SendKeys(username);
+            }
 
             // Password
             var passwordField = driver.FindElement(By.Id("password"));
-
             passwordField.Clear();
+            if (!string.IsNullOrEmpty(password))
+            {
+                passwordField.SendKeys(password);
+            }
 
             // Click Login button
             var loginButton = driver.FindElement(By.Id("login-button"));
--- Test_Automation_Final_Task/UnitTest1.cs
+++ Test_Automation_Final_Task/UnitTest1.cs
@@ -1,90 +1,109 @@
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
 using Test_Automation_Final_Task.Pages;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Test_Automation_Final_Task
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
+        private IWebDriver driver;
+        private readonly Dictionary<string, string> browserSettings;
 
+        public Tests()
+        {
+            browserSettings = LoadBrowserSettings();
         }
 
-        [Test]
-        public void TestLoginFormWithEmptyCredentials()
+        private Dictionary<string, string> LoadBrowserSettings()
         {
-            IWebDriver driver = new ChromeDriver();
-            try
-            {
-                var loginPageEmpty = new LoginPageEmpty(driver);
-                loginPageEmpty.Open().Login();
+            string settingPath = "C:\\Users\\mozi1\\source\\repos\\Test_Automation_Final_Task\\Test_Automation_Final_Task\\settings\\browserSettings.json";
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(settingPath));
+        }
 
-                string actualErrorMessage = driver.FindElement(By.CssSelector("h3[data-test='error']")).Text;
-                string expectedErrorMessage = "Epic sadface: Username is required";
-
-                Assert.That(actualErrorMessage, Is.EqualTo(expectedErrorMessage), "Error message does not match.");
-            }
-            catch (Exception ex)
+        public static IEnumerable<string> GetBrowsersFromJson()
+        {
+            var browserConfig = new Tests().LoadBrowserSettings();
+            foreach (var browserName in browserConfig.Keys)
             {
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-            finally
-            {
-                driver.Quit();
+                yield return browserName;
             }
         }
 
-        [Test]
-        public void TestLoginFormWithUsernameNoPassword()
+        private IWebDriver CreateDriver(string browserName)
         {
-            IWebDriver driver = new ChromeDriver();
-            try
+            return browserName switch
             {
-                var loginPageUsername = new LoginPageUsername(driver);
-                loginPageUsername.Open().Login("standard_user");
+                "Edge" => new EdgeDriver(),
+                "Firefox" => new FirefoxDriver(),
+                "Chrome" => new ChromeDriver(),
+                _ => throw new Exception("Unsupported browser name"),
+            };
+        }
 
-                string actualErrorMessage = driver.FindElement(By.CssSelector("h3[data-test='error']")).Text;
-                string expectedErrorMessage = "Epic sadface: Password is required";
+        public void Setup(string browserName)
+        {
+            driver = CreateDriver(browserName);
+        }
 
-                Assert.That(actualErrorMessage, Is.EqualTo(expectedErrorMessage), "Error message does not match.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-            finally
-            {
-                driver.Quit();
-            }
+        private void AssertErrorMessage(string actual, string expected)
+        {
+            Assert.That(actual, Is.EqualTo(expected), "Error message does not match.");
         }
 
         [Test]
-        public void TestLoginFormWithCredentials()
+        [TestCaseSource(nameof(GetBrowsersFromJson))]
+        public void TestLoginFormWithEmptyCredentials(string browserName)
         {
-            IWebDriver driver = new ChromeDriver();
-            try
-            {
-                var loginPageCorrect = new LoginPageCorrect(driver);
-                loginPageCorrect.Open().Login("standard_user", "secret_sauce");
+            Setup(browserName);
 
-                string actualLogoMessage = driver.FindElement(By.ClassName("app_logo")).Text;
-                string expectedLogoMessage = "Swag Labs";
+            var loginPage = new LoginPage(driver);
+            loginPage.Open().Login();
 
-                Assert.That(actualLogoMessage, Is.EqualTo(expectedLogoMessage), "Error message does not match.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-            finally
-            {
-                driver.Quit();
-            }
+            string actualErrorMessage = driver.FindElement(By.CssSelector("h3[data-test='error']")).Text;
+            string expectedErrorMessage = "Epic sadface: Username is required";
+
+            AssertErrorMessage(actualErrorMessage, expectedErrorMessage);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetBrowsersFromJson))]
+        public void TestLoginFormWithUsernameNoPassword(string browserName)
+        {
+            Setup(browserName);
+
+            var loginPage = new LoginPageUsername(driver);
+            loginPage.Open().Login("standard_user");
+
+            string actualErrorMessage = driver.FindElement(By.CssSelector("h3[data-test='error']")).Text;
+            string expectedErrorMessage = "Epic sadface: Password is required";
+
+            AssertErrorMessage(actualErrorMessage, expectedErrorMessage);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetBrowsersFromJson))]
+        public void TestLoginFormWithCredentials(string browserName)
+        {
+            Setup(browserName);
+
+            var loginPage = new LoginPage(driver);
+            loginPage.Open().Login("standard_user", "secret_sauce");
+
+            string actualLogoMessage = driver.FindElement(By.ClassName("app_logo")).Text;
+            string expectedLogoMessage = "Swag Labs";
+
+            AssertErrorMessage(actualLogoMessage, expectedLogoMessage);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver?.Quit();
+            driver?.Dispose();
         }
     }
 }
